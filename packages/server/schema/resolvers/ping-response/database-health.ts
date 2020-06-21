d1f35b6a1009d0299c633e1ce348ae7097b549ca
Ryan Poe <baublet@gmail.com>
2020-06-21 13:54:08 -0500

    Document the database health check

--- packages/server/schema/resolvers/ping-response/database-health.ts
+++ packages/server/schema/resolvers/ping-response/database-health.ts
@@ -1,5 +1,12 @@
 import { Context } from "../../context";
+import { log } from "../../../utilities";
 
+/**
+ * Our general database health check. This actually establishes a connection
+ * to the database, checks truthiness, tries to load a dataLoader, checks
+ * truthiness, and if any part of that throws is fails their checks, we
+ * send back "red". Otherwise, we're green!
+ */
 export async function databaseHealth(
   _: unknown,
   __: unknown,
@@ -7,9 +14,22 @@ export async function databaseHealth(
 ): Promise<"red" | "green"> {
   try {
     const connection = await context.getConnection();
-    if (connection as any) return "green";
-    return "red";
+    if (!connection as any) {
+      log.error(
+        "Unknown error: falsy connection returned from getConnection()"
+      );
+      return "red";
+    }
+    const userLoader = context.loaders.user();
+    if (!userLoader) {
+      log.error(
+        "Unknown error: falsy user loader returned from context.loaders.user()"
+      );
+      return "red";
+    }
+    return "green";
   } catch (e) {
+    log.error("Database error!", e);
     return "red";
   }
 }
--- packages/server/schema/resolvers/ping-response/response.ts
+++ packages/server/schema/resolvers/ping-response/response.ts
@@ -1,3 +1,6 @@
+/**
+ * Super basic health check that the server is even responding.
+ */
 export function response() {
-  return "pong"
+  return "pong";
 }
